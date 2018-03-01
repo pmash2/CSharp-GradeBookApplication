1727e59862af5de73bb5636b330ee94c7d1d95f7
Phil Ashton <phillipashton@quadax.com>
2018-03-01 10:33:03 -0500

    Create CalculateStatistics Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,6 +11,16 @@ namespace GradeBook.GradeBooks
       Type = GradeBookType.Ranked;
     }
 
+    public override void CalculateStatistics()
+    {
+      if (Students.Count < 5)
+      {
+        System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+        return;
+      }
+      base.CalculateStatistics();
+    }
+
     public override char GetLetterGrade(double averageGrade)
     {
       if (Students.Count < 5)
