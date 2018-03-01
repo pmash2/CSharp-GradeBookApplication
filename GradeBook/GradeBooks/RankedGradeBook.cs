1d8c45b865405fefeb1b739e64765e3f8795aba4
Phil Ashton <phillipashton@quadax.com>
2018-03-01 10:13:57 -0500

    Determine ranked letter grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 using GradeBook.Enums;
 using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -17,7 +18,22 @@ namespace GradeBook.GradeBooks
         throw new InvalidOperationException("Ranked grading requires at least 5 students");
       }
 
-      return 'F';
+      // Number of students that should receive each letter grade
+      var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+
+      //                Order students by       their avg grade   and take their avg grade val  to a list
+      var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+      
+      if (grades[threshold - 1] <= averageGrade)
+        return 'A';
+      else if (grades[(threshold * 2) - 1] <= averageGrade)
+        return 'B';
+      else if (grades[(threshold * 3) - 1] <= averageGrade)
+        return 'C';
+      else if (grades[(threshold * 4) - 1] <= averageGrade)
+        return 'D';
+      else
+        return 'F';
     }
   }
 }
